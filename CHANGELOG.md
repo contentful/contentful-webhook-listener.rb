8dc3a88e2609b2de3975aac2a9c2dbcfcba09a97
David Litvak Bruno <david.litvakb@gmail.com>
2015-11-11 15:10:52 -0300

    Bump version to 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 # Changelog
 
 ## Unreleased
+
+## v0.1.0
 ### Added
 * Full Spec Coverage
 
--- lib/contentful/webhook/listener/version.rb
+++ lib/contentful/webhook/listener/version.rb
@@ -2,7 +2,7 @@ module Contentful
   module Webhook
     # Webhook Listener
     module Listener
-      VERSION = '0.0.4'
+      VERSION = '0.1.0'
     end
   end
 end
